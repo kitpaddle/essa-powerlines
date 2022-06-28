7f18c85bfdcce8d2670580eb600360cd57cd5ff7
kitpaddle <49337981+kitpaddle@users.noreply.github.com>
2022-06-28 07:49:19 +0200

    Update script.js
    Several big changes.
    
    Added saving and reading of JSON file. So it saves the "application" and a user can upload it. Including the geojson data and user data.
    Also fixed minor bugs and added more comments in code. 
    Also changed size of infobar on the right.
    Also added resize capabilities for when window is resized

--- script.js
+++ script.js
@@ -1,14 +1,32 @@
+// SIZE MAP AND INTERFACE TO WINDOW
+
 let viewport_width = document.documentElement.clientWidth;
 let viewport_height = document.documentElement.clientHeight;
 document.getElementById('main').style.width = viewport_width+'px';
 document.getElementById('main').style.height = viewport_height+'px';
 document.getElementById('tooltip').style.height = viewport_height-80+'px';
 
+// Add listener to resize when window resizes.
+window.addEventListener('resize', function(event) {
+  viewport_width = document.documentElement.clientWidth;
+  viewport_height = document.documentElement.clientHeight;
+  document.getElementById('main').style.width = viewport_width+'px';
+  document.getElementById('main').style.height = viewport_height+'px';
+  document.getElementById('tooltip').style.height = viewport_height-80+'px';
+}, true);
+
+// Variables
+let savedData = {};
+let exportJson = {};
+let selectedFeatures = [];
+let bounds;
+
 //// JS related to TOOLTIP
 function hover() {
   let tt = document.getElementById('tooltip');
   tt.style.visibility = "visible";
-}https://codepen.io/kitpaddle/full/yLvjrVV
+}
+
 function leave() {
   let tt = document.getElementById('tooltip');
   tt.style.visibility = "hidden";
@@ -27,6 +45,7 @@ const map = L.map('map', {
   center: startingPos,
   zoom: 10,
   fullscreenControl: true,
+  fullscreenControlOptions: {position: 'topleft'},
   attributionControl: false,
   renderer: L.canvas()
 });
@@ -108,8 +127,6 @@ let geoCtrPoly = {
           ]
       }
 };
-
-let selectedFeatures = [];
   
 //Creating Layers from GeoJson
 let ctrLayer = new L.geoJSON(geoCtrPoly, {pmIgnore: true, interactive: false, style:{opacity:0.4, fillOpacity: 0.1}});
@@ -143,6 +160,7 @@ fetch('https://kitpaddle.github.io/hosting/M20-1-000-00-P3.json').then(response 
   //console.log(layersArray);
   essaLayer = new L.geoJSON(filteredData, {pmIgnore: true, style:{color:'black'}});
   essaLayer.addTo(map);
+  bounds = essaLayer.getBounds();
   panelLayers.addOverlay(essaLayer, "Runways");
 }).catch(err => {
   // Do something for an error here
@@ -167,19 +185,19 @@ map.pm.disableDraw();
 map.pm.setPathOptions({
   color: 'orange',
   weight: 6,
-  fillOpacity: 1,
+  fillOpacity: 1
 });
 
 // Rewriting Geojson everytime a change is made to show tooltips
 map.on('pm:create', (e) => {
-  let temp = map.pm.getGeomanDrawLayers(true);
+  let temp = map.pm.getGeomanLayers(true);
   console.log('Added powerline');
   let temp2 = temp.toGeoJSON();
   drawNames(temp2);
 });
 
 map.on('pm:remove', (e) => {
-  let temp = map.pm.getGeomanDrawLayers(true);
+  let temp = map.pm.getGeomanLayers(true);
   console.log('Removed powerline');
   let temp2 = temp.toGeoJSON();
   drawNames(temp2);
@@ -190,7 +208,7 @@ let nameLayer = new L.geoJSON(null,{pmIgnore: true});
 nameLayer.addTo(map);
 
 // Function to Drawnames on geojson with tooltip
-let exportJson;
+
 function drawNames(json){
   exportJson = json;
   map.removeLayer(nameLayer);
@@ -212,12 +230,23 @@ function onEachPower(feature, layer){
 //// FUNCTION TO CREATE .JSON GeoJson file with drawn powerlines
 
 function download(content, fileName, contentType) {
-    var a = document.createElement("a");
-    var file = new Blob([JSON.stringify(content, null, 2)], {type: contentType});
-    a.href = URL.createObjectURL(file);
-    a.download = fileName;
-    a.click();
-    URL.revokeObjectURL(a.href);
+  let form = document.getElementById('survey-form');
+  content.fullname = form.elements['name'].value;
+  content.phone = form.elements['phone'].value;
+  content.email = form.elements['email'].value;
+  content.actype = form.elements['actype'].value;
+  content.callsign = form.elements['accall'].value;
+  content.date = form.elements['dof'].value;
+  content.extrainfo = form.elements['comments'].value;
+  if(form.elements['alt'].checked) content.altitude = 'YES';
+  else content.altitude = 'NO';
+  
+  var a = document.createElement("a");
+  var file = new Blob([JSON.stringify(content, null, 2)], {type: contentType});
+  a.href = URL.createObjectURL(file);
+  a.download = fileName;
+  a.click();
+  URL.revokeObjectURL(a.href);
 }
 
 //// PRINTING Application and creating json file
@@ -226,7 +255,7 @@ let form = document.getElementById('survey-form');
 form.addEventListener('submit', (event) => {
   event.preventDefault(); //Don't open new page
   
-  let bounds = essaLayer.getBounds();
+  
   bounds.extend(map.pm.getGeomanDrawLayers(true).getBounds());
   baseMap.addTo(map);
   
@@ -238,7 +267,7 @@ form.addEventListener('submit', (event) => {
   document.getElementById('main').style.width = '400mm';
   document.getElementById('map').style.width = '100%';
   
-  // FIlling in data from FORM
+  // Filling in data from FORM
   document.getElementById('printname').innerHTML = form.elements['name'].value;
   document.getElementById('printnr').innerHTML = form.elements['phone'].value;
   document.getElementById('printemail').innerHTML = form.elements['email'].value;
@@ -272,3 +301,52 @@ form.addEventListener('submit', (event) => {
   download(exportJson, datenow+'_'+form.elements['accall'].value+'.json', 'text/plain');
   
 });
+
+///////////// LOAD JSON FILE ///////////
+
+// Called when choosing the file
+function checkFile(){
+  let file = document.getElementById('fileInput').files[0];
+  // Extra check of file size and or type?
+  //console.log(file.size);
+  //console.log(file.type);
+}
+
+// Called by the "LOAD FILE..." button
+function loadJson(){
+  let selected = document.getElementById('fileInput').files[0];
+  
+  // READ SELECTED FILE
+  let reader = new FileReader();
+  reader.addEventListener("loadend", () => {
+    savedData = JSON.parse(reader.result);
+    loadPageFromData();
+  });
+  reader.readAsText(selected);
+}
+
+function loadPageFromData(){
+  // Filling in FORM from DATA
+  document.getElementById('name').value = savedData.fullname;
+  document.getElementById('phone').value = savedData.phone;
+  document.getElementById('email').value = savedData.email;
+  document.getElementById('actype').value = savedData.actype;
+  document.getElementById('accall').value = savedData.callsign;
+  document.getElementById('dof').value = savedData.date;
+  document.getElementById('comments').value = savedData.extrainfo;
+  //document.getElementById('printlines').innerHTML = 
+  if(savedData.altitude == "YES") document.getElementById('altCheck').checked = true;
+  else document.getElementById('altCheck').checked = false;
+  
+  nameLayer.remove();
+  map.pm.getGeomanLayers(true).eachLayer(function(layer){
+    layer.pm.remove();
+  });
+  
+  
+  let loadedLayer = new L.geoJSON(savedData, {style:{color: 'orange', weight: 6, fillOpacity: 1}});
+  loadedLayer.addTo(map);
+  bounds.extend(loadedLayer.getBounds());
+  drawNames(savedData);
+  
+}
